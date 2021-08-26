b6f1f43abf170d8625ee97c33465843030cdff59
gualtierigio <gualtierigio@gmail.com>
2021-08-26 15:38:05 +0200

    inizzializzazione esercizio cognomi

---
+++ javascript/myscript.js
@@ -0,0 +1,8 @@
+/*
+ * 
+ * chiedi all’utente il cognome
+ * inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+ * stampa la lista ordinata alfabeticamente
+ * scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+ * 
+ */
